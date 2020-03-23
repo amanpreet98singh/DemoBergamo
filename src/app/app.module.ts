637faac77025ed8073edbcaf51cef081c25081bc
amanpreet98singh <57998412+amanpreet98singh@users.noreply.github.com>
2020-03-23 16:41:57 +0100

    configurazione router moduls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PrimoComponent } from './primo/primo.component';
 import { SecondoComponent } from './secondo/secondo.component';
 import { CiclovitaComponent } from './ciclovita/ciclovita.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'primo', component: PrimoComponent, data:{title:"Primo"} },
+  { path: 'primo-diverso', component: PrimoComponent, data:{title:"Primo diverso"} },
+  { path: 'secondo',      component: SecondoComponent },
+  { path: 'ciclo-vita',      component: CiclovitaComponent },
+  { path: '', redirectTo: '/primo', pathMatch:'full' },
+  {path: '**', component:PageNotFoundComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimoComponent,
     SecondoComponent,
-    CiclovitaComponent
+    CiclovitaComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
